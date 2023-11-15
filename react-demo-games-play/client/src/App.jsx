a7842eeab835c8442d05f711bf83e2fe062ad138
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-11-15 21:58:21 +0200

    Added Create component

--- react-demo-games-play/client/src/App.jsx
+++ react-demo-games-play/client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Catalog from './components/catalog/Catalog';
+import Create from './components/create/Create';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/catalog" element={<Catalog/>} />
+        <Route path="/create" element={<Create/>} />
       </Routes>
     </div>
   )
