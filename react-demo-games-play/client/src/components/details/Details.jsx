4aa3268d85385e899fad1ceaa8b2cd47be6a6b8a
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-11-19 10:32:59 +0200

    Add new comments to state

--- react-demo-games-play/client/src/components/details/Details.jsx
+++ react-demo-games-play/client/src/components/details/Details.jsx
@@ -2,20 +2,38 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import * as gameService from '../../services/gameService';
+import * as commentService  from '../../services/commentService';
+
+// should create controlled forms
 
 export default function Details(){
     const [game, setGame] = useState({});
+    const [comments, setComments] = useState([]);
     const { gameId } = useParams();
-
 
     // service layer
     useEffect(() => {
         gameService.getOne(gameId)
             .then(setGame);
+
+        commentService.getAll()
+            .then(setComments);
     }, [gameId]);
 
-    const addCommentHandler = () => {
-        
+    const addCommentHandler = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const newComment = await commentService.create(
+            gameId,
+            formData.get('username'),
+            formData.get('comment')
+        );
+
+        setComments(state => [...state, newComment]);
+        //console.log(newComment);
+
     }
 
     return (
@@ -32,28 +50,30 @@ export default function Details(){
 
                 <p className="text">{game.summary}</p>
 
-                {/* <div className="details-comments">
+                <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
+                        {comments.map(({_id, username, text}) => (
+                            <li key={_id} className="comment">
+                                <p>{username} : {text}</p>
+                            </li>
+                        ))}
                     </ul>
-                    <p className="no-comment">No comments.</p>
+                    {comments.length === 0 && (
+                        <p className="no-comment">No comments.</p>
+                    )}
                 </div>
 
                 <div className="buttons">
                     <a href="#" className="button">Edit</a>
                     <a href="#" className="button">Delete</a>
-                </div> */}
+                </div>
             </div>
 
             <article className="create-comment">
                 <label>Add new comment:</label>
-                <form className="form">
+                <form className="form" onSubmit={addCommentHandler}>
+                    <input type="text" name="username" placeholder="Username"/>
                     <textarea name="comment" placeholder="Comment......"></textarea>
                     <input className="btn submit" type="submit" value="Add Comment" />
                 </form>
